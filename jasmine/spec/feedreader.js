2a0ae97a9189c76d0ac6afc1cb008dfda0ae319e
David van Oudheusden <david@dedaaf.com>
2016-01-10 21:05:38 +0100

    beautified js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,16 +3,15 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -30,7 +29,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('Feed URL is defined and not Empty', function(){
+        it('Feed URL is defined and not Empty', function() {
 
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeDefined();
@@ -42,7 +41,7 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('Feed name is defined and not empty', function(){
+        it('Feed name is defined and not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toBe(0);
@@ -63,16 +62,16 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        it('The slide menu is hidden by default',function(){
-            expect( body).toBe(true);
+        it('The slide menu is hidden by default', function() {
+            expect(body).toBe(true);
         });
 
         /* Test that ensures the menu changes
-        * visibility when the menu icon is clicked. This test
-        * should have two expectations: does the menu display when
-        * clicked and does it hide when clicked again.
-        */
-        it('Menu toggles when icon is clicked',function(){
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+        it('Menu toggles when icon is clicked', function() {
             btn.click();
             expect($('body').attr('class')).toEqual('');
             btn.click();
@@ -89,13 +88,13 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        beforeEach(function(done){
-            loadFeed(0,function(){//activate the feed
+        beforeEach(function(done) {
+            loadFeed(0, function() { //activate the feed
                 done();
             });
         });
 
-        it('at least a single entry element is found', function(){
+        it('at least a single entry element is found', function() {
             expect($('.entry').length).toBeGreaterThan(0);
         });
     });
@@ -103,12 +102,12 @@ $(function() {
     /* Test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
         /* This function checks if a new news item is loaded
-                 */
-       var feed_title;
-       var feed_title_new;
+         */
+        var feed_title;
+        var feed_title_new;
 
         beforeEach(function(done) {
-            loadFeed(0,function(){//activate the feed
+            loadFeed(0, function() { //activate the feed
                 done();
 
             });
@@ -119,12 +118,12 @@ $(function() {
 
         it("New Feed is loaded", function(done) {
 
-            loadFeed(0,function(){//activate the feed
+            loadFeed(0, function() { //activate the feed
                 done();
             });
             var feed_title_new = $('.entry h2')[0].innerText;
 
-            expect(feed_title_new).toEqual (feed_title);
+            expect(feed_title_new).toEqual(feed_title);
             console.log('Second title: ', feed_title_new);
         });
 
